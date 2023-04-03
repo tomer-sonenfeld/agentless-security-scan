9aff06d14c68857d99f87a23231dec908e46cde4
tomerson <tomerson@bgu.ac.il>
2023-04-03 18:44:56 +0300

    fetching

---
+++ fetch_vms.py
@@ -0,0 +1,7 @@
+from azure.mgmt.compute import ComputeManagementClient
+from azure.identity import DefaultAzureCredential
+credential = DefaultAzureCredential()
+compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+vms = compute_client.virtual_machines.list_all()
+vms_list=[v for v in vms]
+print(vms_list)
