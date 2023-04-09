a3677092f691df57e0cf6a7542bc92cf86d4d6d5
tomerson <tomerson@bgu.ac.il>
2023-04-09 14:18:48 +0300

    found the real vm

--- azure-scripts/get_vm_of_disk.py
+++ azure-scripts/get_vm_of_disk.py
@@ -4,4 +4,13 @@ credential = DefaultAzureCredential()
 compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
 disks = [d for d in compute_client.disks.list()]
 disk = disks[2]
-print("This disk belongs to vm = "+disk.managed_by)
+vm_name = disk.managed_by.split('/')[-1]
+
+print("This disk belongs to vm = "+vm_name)
+
+vms = compute_client.virtual_machines.list_all()
+vms_list=[v for v in vms]
+
+for vm in vms_list:
+    if vm.name == vm_name:
+        print("VM details = "+str(vm))
