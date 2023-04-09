efbc68ad9d0340e1c72e67dfedb92c0eac66649c
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2023-04-09 15:15:24 +0300

    update cpu script to better version

--- nir_azure_scripts/get_cpu_num_of_vm.py
+++ nir_azure_scripts/get_cpu_num_of_vm.py
@@ -5,9 +5,19 @@ from azure.identity import DefaultAzureCredential
 credential = DefaultAzureCredential()
 compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
 
+
+
+location = "uksouth"
+vs_sizes = [s for s in compute_client.virtual_machine_sizes.list(location)]
+
 vms = [v for v in compute_client.virtual_machines.list_all()]
 v0 = vms[0]
-vm_size = v0.hardware_profile.vm_size
-cpu_regex = "(?<=_D)\d+"
-num_cpus = re.findall(cpu_regex, vm_size)[0]
-print("number of cpus for vm0 is: "+str(num_cpus))
+vm_size_name = v0.hardware_profile.vm_size
+for vs_size in vs_sizes:
+    if vs_size.name == vm_size_name:
+        vm_size = vs_size
+        break
+
+num_cpus = vm_size.number_of_cores
+ram_size = vm_size.memory_in_mb
+print(f"number of cpus is: {num_cpus} and ram size is: {ram_size}")
