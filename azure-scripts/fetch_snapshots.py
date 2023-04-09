6e07b91f3a10adffeb0a10bc18557b33c0b45e46
tomerson <tomerson@bgu.ac.il>
2023-04-09 13:40:49 +0300

    test

--- azure-scripts/fetch_snapshots.py
+++ azure-scripts/fetch_snapshots.py
@@ -4,4 +4,3 @@ credential = DefaultAzureCredential()
 compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
 snapshots = [s for s in compute_client.snapshots.list()]
 print("snpshots= "+str([s.name for s in snapshots]))
-print("hello world")
