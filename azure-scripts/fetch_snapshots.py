2f38de1bc2c265541aa5f43c5b92dc136d2460e1
tomerson <tomerson@bgu.ac.il>
2023-04-09 13:17:42 +0300

    names

---
+++ azure-scripts/fetch_snapshots.py
@@ -0,0 +1,6 @@
+from azure.mgmt.compute import ComputeManagementClient
+from azure.identity import DefaultAzureCredential
+credential = DefaultAzureCredential()
+compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+snapshots = [s for s in compute_client.snapshots.list()]
+print("snpshots= "+str([s.name for s in snapshots]))
