712d5261da63e0f0519e985d4b92d67ea21b43e0
nirrose <nirrose@post.bgu.ac.il>
2023-04-17 12:45:21 +0300

    added os type

--- nir_azure_scripts/get_architecture_for_vm.py
+++ nir_azure_scripts/get_architecture_for_vm.py
@@ -8,7 +8,10 @@ resource_group_name = "StartShip"
 
 vms = [v for v in compute_client.virtual_machines.list_all()]
 v0 = vms[0]
-disk_name = v0.storage_profile.os_disk_name
+disk_name = v0.storage_profile.os_disk.name
 d = compute_client.disks.get(resource_group_name, disk_name)
 arc = d.supported_capabilities.architecture
+print("os_type from virtual machine is : "+v0.storage_profile.os_disk.os_type)
 print(arc) # arm processors start with Arm__, for exg. Arm64 default is AMD
+
+
