5754376f412213a0d7a9eb68bdeb85ae23dd975b
Saar <saarbu@post.bgu.ac.il>
2023-04-17 17:03:47 +0300

    Fixed update and added tags removal, tag update

--- add_tags.py
+++ add_tags.py
@@ -9,7 +9,9 @@ resource_client = ResourceManagementClient(credential,
                                            subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
 vm = compute_client.virtual_machines.get(resource_group_name='StartShip',
                                          vm_name='test-auth')
-vm.tags.update({'test_tag1': 'tagged'})
+vm.tags.update({'test_tag2': 'tagged'})
+vm.tags.pop('test_tag1', None)
+vm.tags.update({'test_tag2': 'tagged_updated'})
 compute_client.virtual_machines.begin_create_or_update(resource_group_name='StartShip',
                                                        vm_name='test-auth',
                                                        parameters=vm)
