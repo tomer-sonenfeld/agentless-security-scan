cee68050a0210eaa93f1c50c0346e4fc68bbf72e
nirrose <nirrose@post.bgu.ac.il>
2023-04-17 11:39:17 +0300

    update

--- fetch_vms.py
+++ fetch_vms.py
@@ -1,7 +1,8 @@
 from azure.mgmt.compute import ComputeManagementClient
 from azure.identity import DefaultAzureCredential
 credential = DefaultAzureCredential()
-compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+subscription_id ='d92e7f13-b7ac-4ace-af5a-562f364d6c45'
+compute_client = ComputeManagementClient(credential, subscription_id)
 vms = compute_client.virtual_machines.list_all()
 vms_list=[v for v in vms]
 print(vms_list)
