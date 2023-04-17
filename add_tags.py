10804de62e889a37c18b29ff33d58b93b1bbc0e1
Saar <saarbu@post.bgu.ac.il>
2023-04-17 15:24:35 +0300

    add tags script test

---
+++ add_tags.py
@@ -0,0 +1,12 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.resources.models import GenericResource
+
+
+credential = DefaultAzureCredential()
+compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+resource_client = ResourceManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+vm = compute_client.virtual_machines.get(resource_group_name='StartShip', vm_name='test-auth')
+vm.tags.update({'test_tag1': 'tagged'})
+compute_client.virtual_machines.create_or_update(resource_group_name='StartShip', vm_name='test-auth', vm)
