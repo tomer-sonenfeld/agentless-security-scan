7a6e51604a83addbb701d7aeb4544e903de8eae2
Saar <saarbu@post.bgu.ac.il>
2023-04-17 16:17:59 +0300

    fixed typo

--- add_tags.py
+++ add_tags.py
@@ -1,12 +1,15 @@
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.resource import ResourceManagementClient
-from azure.mgmt.resource.resources.models import GenericResource
-
 
 credential = DefaultAzureCredential()
-compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
-resource_client = ResourceManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
-vm = compute_client.virtual_machines.get(resource_group_name='StartShip', vm_name='test-auth')
+compute_client = ComputeManagementClient(credential,
+                                         subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+resource_client = ResourceManagementClient(credential,
+                                           subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
+vm = compute_client.virtual_machines.get(resource_group_name='StartShip',
+                                         vm_name='test-auth')
 vm.tags.update({'test_tag1': 'tagged'})
-compute_client.virtual_machines.create_or_update(resource_group_name='StartShip', vm_name='test-auth', vm)
+compute_client.virtual_machines.begin_create_or_update(resource_group_name='StartShip',
+                                                       vm_name='test-auth',
+                                                       parameters=vm)
