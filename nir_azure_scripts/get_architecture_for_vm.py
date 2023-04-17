44ad0fe85cc28cb7c81bcc80dc21c88b96200b4b
nirrose <nirrose@post.bgu.ac.il>
2023-04-17 11:40:57 +0300

    be updatedgit branch!

---
+++ nir_azure_scripts/get_architecture_for_vm.py
@@ -0,0 +1,14 @@
+from azure.mgmt.compute import ComputeManagementClient
+from azure.identity import DefaultAzureCredential
+
+subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45'
+credential = DefaultAzureCredential()
+compute_client = ComputeManagementClient(credential, subscription_id)
+resource_group_name = "StartShip"
+
+vms = [v for v in compute_client.virtual_machines.list_all()]
+v0 = vms[0]
+disk_name = v0.storage_profile.os_disk_name
+d = compute_client.disks.get(resource_group_name, disk_name)
+arc = d.supported_capabilities.architecture
+print(arc) # arm processors start with Arm__, for exg. Arm64 default is AMD
