c3cdf5f116e28380bead3cfb5d6fe5ef1cfb1ccf
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2023-04-23 10:14:34 +0300

    slight script modifications

--- nir_azure_scripts/get_cpu_num_of_vm.py
+++ nir_azure_scripts/get_cpu_num_of_vm.py
@@ -1,11 +1,8 @@
 import re
 from azure.mgmt.compute import ComputeManagementClient
 from azure.identity import DefaultAzureCredential
-
 credential = DefaultAzureCredential()
 compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
-
-
 
 location = "uksouth"
 vs_sizes = [s for s in compute_client.virtual_machine_sizes.list(location)]
--- nir_azure_scripts/get_ram_of_vm.py
+++ nir_azure_scripts/get_ram_of_vm.py
@@ -2,11 +2,9 @@ import re
 from azure.mgmt.compute import ComputeManagementClient
 from azure.identity import DefaultAzureCredential
 
+subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45'
 credential = DefaultAzureCredential()
-compute_client = ComputeManagementClient(credential, subscription_id='d92e7f13-b7ac-4ace-af5a-562f364d6c45')
-
-
-
+compute_client = ComputeManagementClient(credential, subscription_id)
 location = "uksouth"
 vs_sizes = [s for s in compute_client.virtual_machine_sizes.list(location)]
 
@@ -17,7 +15,3 @@ for vs_size in vs_sizes:
     if vs_size.name == vm_size_name:
         vm_size = vs_size
         break
-
-num_cpus = vm_size.number_of_cores
-ram_size = vm_size.memory_in_mb
-print(f"ram size is: {ram_size}")
